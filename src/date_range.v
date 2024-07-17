812f419380a0fa5aacf68f5f596835dea90072e9
TahaHachana <tahahachana@gmail.com>
2024-07-17 07:18:39 +0100

    Add past n months range calculation

---
+++ src/date_range.v
@@ -0,0 +1,56 @@
+module main
+
+import time
+
+
+struct Month {
+	month int
+	year  int
+}
+
+struct DateRange {
+	from time.Time
+	to   time.Time
+}
+
+fn (m Month) full_month_date_range() DateRange {
+	days_in_month := time.days_in_month(m.month, m.year) or { panic(err) }
+
+	from := time.Time{
+		year: m.year
+		month: m.month
+		day: 1
+	}
+
+	to := time.Time{
+		year: m.year
+		month: m.month
+		day: days_in_month
+	}
+
+	return DateRange{
+		from: from
+		to: to
+	}
+}
+
+fn past_n_months(n int) []Month {
+    mut months := []Month{}
+    now := time.now()
+    mut current_month := now.month
+    mut current_year := now.year
+
+    for i := 0; i < n; i++ {
+        if current_month < 1 {
+            current_month = 12
+            current_year--
+        }
+        month := Month{
+            month: current_month
+            year: current_year
+        }
+        months << month
+        current_month--
+    }
+    return months.reverse()
+}
--- src/main.v
+++ src/main.v
@@ -3,37 +3,54 @@ module main
 import os
 import flag
 
-fn main() {
-	mut fp := flag.new_flag_parser(os.args)
+const bd_token_var = 'BRIGHTDATA_API_TOKEN'
 
+fn init_fp() &flag.FlagParser {
+	mut fp := flag.new_flag_parser(os.args)
 	fp.application(name)
 	fp.version(version)
 	fp.description(description)
 	fp.skip_executable()
+	return fp
+}
 
-	env_var := os.read_lines('.env') or { [] }
-	if env_var.len != 0 {
-		os.setenv('BRIGHTDATA_API_TOKEN', env_var[0].split('=')[1], true)
-	}
-	bd_api_token := os.getenv('BRIGHTDATA_API_TOKEN')
-
-	token := fp.string('token', `t`, bd_api_token, 'The Bright Data API token.')
+fn finalize_fp(mut fp flag.FlagParser) ! {
 	additional_args := fp.finalize()!
-
 	if additional_args.len > 0 {
 		println('Unprocessed arguments:\n${additional_args.join_lines()}')
 	}
+}
 
+fn main() {
+	mut fp := init_fp()
+
+	// Set the API token from the environment variable if it exists
+	env_vars := os.read_lines('.env') or { [] }
+	if env_vars.len != 0 {
+		os.setenv(bd_token_var, env_vars[0].split('=')[1], true)
+	}
+	bd_api_token := os.getenv(bd_token_var)
+
+	// Parse the command line arguments
+	token := fp.string('token', `t`, bd_api_token, 'The Bright Data API token.')
+	months := fp.int('months', `m`, 12, 'The number of months to retrieve data for.')
+
+	finalize_fp(mut fp)!
+
+	// Save new token to the environment variable and .env file
 	if token == '' {
 		println('A valid API token is required.')
 		exit(1)
 	} else {
 		if token != bd_api_token {
-			os.setenv('BRIGHTDATA_API_TOKEN', token, true)
+			os.setenv(bd_token_var, token, true)
 			text := 'BRIGHTDATA_API_TOKEN=${token}'
 			os.write_file('.env', text)!
 		}
 	}
 
-	println('Token: ${os.getenv('BRIGHTDATA_API_TOKEN')}')
+	// Get the date ranges for the past n months
+	date_ranges := past_n_months(months).map(|m| m.full_month_date_range())
+
+	println(date_ranges)
 }
